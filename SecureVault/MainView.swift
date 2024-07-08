b22a96600919eca0eff024fdee5bf7aa21bbc14d
waradsanath <warad_sanath_manjunath@s2023.ssts.edu.sg>
2024-07-08 16:12:09 +0800

    did stuff

--- SecureVault/MainView.swift
+++ SecureVault/MainView.swift
@@ -9,18 +9,24 @@ import SwiftUI
 
 struct MainView: View {
     @State private var showingSheet = false
+    @AppStorage("isDarkMode") var isDarkMode: Bool = false
     var body: some View {
         NavigationStack {
             Text("")
                 .navigationTitle("Home Page")
-                .toolbar(content: {
+                .toolbar {
                     Button("Info") {
                         showingSheet.toggle()
                     }
                     .sheet(isPresented: $showingSheet) {
                         HelpView()
                     }
-                })
+                    Button {
+                        isDarkMode.toggle()
+                    } label: {
+                        isDarkMode ? Image(systemName: "moon.stars") : Image(systemName: "sun.max")
+                    }
+                }
         }
     }
 }
---
+++ SecureVault/SecureVault/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  SecureVault
+//
+//  Created by Sanath Warad on 8/7/24.
+//
+
+import Foundation
+
+struct User: Identifiable {
+    var id = 
+}
